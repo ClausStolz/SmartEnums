0e2ed8b1c65e24f946c0df875c92f6a880a250d3
Claus Stolz <clausstolz@yandex.ru>
2022-01-02 19:09:55 +0300

    Init version for attribute

--- SmartEnums.Core/Attributes/EnumValueAttribute.cs
+++ SmartEnums.Core/Attributes/EnumValueAttribute.cs
@@ -9,11 +9,14 @@ namespace SmartEnums
         public string Key { get; }
 
         public object Value { get; }
+        
+        public string Version { get; }
 
-        public EnumValueAttribute(string key, object value)
+        public EnumValueAttribute(string key, object value, string version = "latest")
         {
             Key = key;
             Value = value;
+            Version = version;
         }
     }
 }
