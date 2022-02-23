b8bc76f8da02ae3dcc6c72c2ac36288fb88619cc
Claus Stolz <clausstolz@yandex.ru>
2022-02-23 22:28:05 +0300

    SE-8 Update documentation

--- SmartEnums/Exceptions/FieldNotImplementException.cs
+++ SmartEnums/Exceptions/FieldNotImplementException.cs
@@ -1,11 +1,17 @@
 using System;
 
+// ReSharper disable once CheckNamespace
 namespace SmartEnums
 {
+    /// <summary>
+    /// Exception throwing when field not implemented.
+    /// </summary>
     public class FieldNotImplementException : Exception
     {
         private const string TextFormat = "Wrong field name '{0}'. Field not implement in '{1}'.";
-
+        
+        /// <param name="key">Field name implemented in metadata field key.</param>
+        /// <param name="value">Enum object.</param>
         public FieldNotImplementException(string key, Enum value)
             : base(string.Format(TextFormat, key, value.GetType() + "." + value)) { }
     }
---
+++ SmartEnums/Helpers/Config.cs
@@ -0,0 +1,27 @@
+// ReSharper disable once CheckNamespace
+namespace SmartEnums.Helpers
+{
+    /// <summary>
+    /// Helper class configured main options for extensions and classes.
+    /// </summary>
+    public static class Config
+    {
+        /// <summary>
+        /// Keywords for checking latest version.
+        /// </summary>
+        public static readonly string[] LatestVersionFlags = new []
+        {
+            "latest", "newest"
+        };
+
+        /// <summary>
+        /// Identifier for up-version.
+        /// </summary>
+        public const char UpVersionFlag = '^';
+
+        /// <summary>
+        /// Identifier for default version.
+        /// </summary>
+        public const string DefaultVersion = "1.0.0";
+    }
+}
